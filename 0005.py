a80209a949407e4e8390186ce85ed2f6c205f812
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-08 16:05:42 +0800

    Add files via upload

---
+++ 0005.py
@@ -0,0 +1,15 @@
+# 练习不同种类的打印方式
+#1
+a = 5
+print(a)
+#2
+print("My name is %s and age is %d"%("Kulamdir",20))
+#3
+print("My name is {0} and age is {1}".format("Kuladmir",20))
+#4
+name = "Kuladmir"
+age = 20
+pf = f'My name is {name} and age is {age}'
+print(pf)
+#5
+print("My name is "+str(name)+" and age is "+str(age))
