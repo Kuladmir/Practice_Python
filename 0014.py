cdaa0e5718ae2c54558a99d3604da3fcc04dbff7
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-17 13:43:02 +0800

    Add files via upload

---
+++ 0014.py
@@ -0,0 +1,13 @@
+a = 0
+sum = 0
+for a in range(1,10,1):#循环 1 2 3 4 5 6 7 8 9
+    sum += a
+print("%d"%sum)
+sum = 0
+for a in range(10):#循环 0 1 2 3 4 5 6 7 8 9
+    sum += a
+print("%d"%sum)
+sum = 0
+for a in range(1,10):#循环 1 2 3 4 5 6 7 8 9
+    sum += a
+print("%d"%sum)
