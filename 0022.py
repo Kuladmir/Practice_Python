6fe269b80371bb64d3b035bd89b7711c9796487f
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-24 22:22:14 +0800

    Add files via upload

---
+++ 0022.py
@@ -0,0 +1,12 @@
+a = int(input("请输入季度（1 2 3 4）"))
+if a>4:
+    print("Error")
+else:
+    if a == 1:
+        print("3 4 5")
+    elif a == 2:
+        print("6 7 8")
+    elif a == 3:
+        print("9 10 11")
+    else:
+        print("12 1 2")
