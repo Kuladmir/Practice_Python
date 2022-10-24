2ae66b9728e42629542abcba708ec507a6045744
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-24 22:23:08 +0800

    Add files via upload

---
+++ 0025.py
@@ -0,0 +1,9 @@
+c = ["retention","retain","pamper","revamp"]
+for i in c:
+    print(i,end=" ")
+print("")
+for i in range(len(c)):
+    print(c[i],end=" ")
+print("")
+for i,ir in enumerate(c):
+    print(i,ir)
