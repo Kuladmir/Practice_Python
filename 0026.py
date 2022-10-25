e4a1dc625d0249ea989e780b9010f599c14e04cb
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-25 21:49:51 +0800

    Add files via upload

---
+++ 0026.py
@@ -0,0 +1,18 @@
+c = ["Kuladmir","Admirmoon",2019,3050,135]
+b = ["yado","indo"]
+c.append("Kulapati")
+print(c)
+c.extend("ROG")
+print(c)
+c.extend(b)
+print(c)
+c.insert(2,"move")
+print(c)
+del c[7]
+print(c)
+c.remove('O')
+print(c)
+c.pop(7)
+print(c)
+
+
