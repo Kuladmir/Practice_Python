1b8cbd1662ec663016595eda91133ddd8ae10c05
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-25 21:50:13 +0800

    Add files via upload

---
+++ 0027.py
@@ -0,0 +1,18 @@
+c = ["Kulapati","Admirmoon","Kuladmir",56,86]
+d = [15,26,37]
+c[3] = 76
+print(c)
+c.reverse()
+print(c)
+b = c.copy()
+print(b)
+a = c.index(76)
+print(a)
+e = sum(d)
+print(e)
+cc = ["Kulapati","Admirmoon","Kuladmir"]
+cc.sort()
+print(cc)
+cc.sort(reverse = True)
+print(cc)
+
