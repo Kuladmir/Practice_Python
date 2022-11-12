b9c93930cc7efa09c5cda68490bb720ce2384288
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-11-12 17:11:00 +0800

    Add files via upload

---
+++ 0043.py
@@ -0,0 +1,14 @@
+def add(a,b,c):
+    ''' 
+    加数1:param a
+    加数2:param b
+    加数3:param c
+    和:return: 
+    '''
+    return a+b+c
+a=int(input("请输入"))
+b=int(input("请输入"))
+c=int(input("请输入"))
+d = add(a,b,c)
+help(add)
+print(d)
