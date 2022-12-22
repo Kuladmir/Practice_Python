30e9625e1b0ac88805d46679e0cf78a6877f4329
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-12-22 21:46:17 +0800

    Add files via upload

---
+++ 0063.py
@@ -0,0 +1,14 @@
+c = {"Kula":"Hello","dictatorial":"专制的"}
+print(c)
+print(c.get("Kuladmir"))
+c["hostile"] = "敌意的"
+print(c)
+c.pop("Kula")
+print(c)
+a = ["abs","math","add"]
+b = ["||","数学","+"]
+c = dict(zip(a,b))
+print(c)
+c = "kuladmir"
+c = set(c)
+print(c)
