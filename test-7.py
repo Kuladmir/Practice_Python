1a5f9de35b080ee8d9f8b8c81f1e30bed08e78fb
Kulapati Admirmoon <109757921+Kuladmir@users.noreply.github.com>
2025-08-20 23:50:03 +0800

    Add files via upload

---
+++ test-7.py
@@ -0,0 +1,8 @@
+s = {154, 456, 4895, 8974}
+t = {154, 465, 4895, 654}
+print("s = {0}".format(s))
+print("t = {0}".format(t))
+print("s - t = {0}".format(s - t))
+print("s & t = {0}".format(s & t))
+print("s ^ t = {0}".format(s ^ t))
+print("s | t = {0}".format(s | t))
---
+++ test-8.py
@@ -0,0 +1,13 @@
+s = set()
+s = {937, 345, 520, 579}
+print(s)
+s.add(965)
+print(s)
+s.remove(937)
+print(s)
+print(345 in s)
+print(len(s))
+print(345 not in s)
+s.clear()
+print(s)
+
